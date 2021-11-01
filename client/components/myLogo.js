0236ffbd930fef9c532fc770ff17caff5c272d3f
Cristian <2cristianmartinez@gmail.com>
2021-11-01 00:44:37 -0400

    [feat]: Core Web Vitals score updates

--- client/components/myLogo.js
+++ client/components/myLogo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Logo = () => (
   <div id="logo">
     <div className="nav-item">
-      <img src="/images/cm-logo-icon-wht.png" className="logo action" />
+      <img src="/images/cm-logo-icon-wht.png" className="logo action" alt="Cristian Martinez" width="170px" height="40px"/>
     </div>
   </div>
 )
